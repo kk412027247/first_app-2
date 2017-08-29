960dbef878b8892fb56f14ec923f774ed3fc92d8
TMD <1@123.com>
2017-08-29 22:32:19 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>去年买了个</Text>
+        <Text>登山包</Text>
+        <Text>超耐磨</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
