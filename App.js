23b31dbe626b5630c02978674573b2df3b43dd45
TMD <1@123.com>
2017-09-02 22:43:45 +0800

    flex盒模型

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry,StyleSheet, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class FlexBox extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>去年买了个</Text>
-        <Text>登山包</Text>
-        <Text>超耐磨</Text>
+        <View style={styles.view1} />
+        <View style={styles.view2}/>
       </View>
+
     );
   }
 }
@@ -16,8 +16,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'gray' ,
+    flexDirection:'row',//设置子组建横向排列，默认是纵向排列
+    //flexWrap:'wrap', //设置子组建溢出的时候换行，默认不换行
+    //justifyContent:'center',//子组建的水平位置
+    //alignItems:'center',//子组建垂直位位置
   },
+  view1:{
+    flex:1, //设置组建大小 1就是100%
+    height:150,
+    width:150,
+    backgroundColor:'red',
+  }  ,
+  view2:{
+    flex:1,
+    height:150,
+    width:150,
+    backgroundColor:'green' ,
+    //alignSelf:'center',//设置自身垂直位置
+
+  }
 });
+
+AppRegistry.registerComponent('FlexBox',()=> FlexBox);
