45d797f4bf2a96d303628d6054504d07b5f6c7fc
TMD <1@123.com>
2017-09-03 01:40:50 +0800

    电商项目基本框架

--- App.js
+++ App.js
@@ -12,15 +12,15 @@ import {
   ListView,
   Alert,
   TouchableHighlight,
-  StatusBar
+  StatusBar,
   } from 'react-native';
 
 export default class FlexBox extends React.Component {
   constructor (props){
     super(props);
     const ds = new ListView.DataSource({
-      rowHasChanged:(r1,r2)=> r1 !== r2
-    });
+      rowHasChanged:(r1,r2) => r1 !== r2
+    });  //创建ListView。DataSource数据源
     this.state = {
       currentPage:0,
       dataSource:ds.cloneWithRows([
@@ -37,37 +37,82 @@ export default class FlexBox extends React.Component {
       ])
     }
   }
+  componentDidMount(){
+    this._startTimer();
+  }
+  componentWillUnMount(){
+    clearInterval(this.interval)
+  }
+
+  _startTimer =() =>{
+    this.interval= setInterval(()=>{
+      nextPage = this.state.currentPage + 1;
+      if(nextPage >= 3){
+        nextPage = 0;
+      }
+      this.setState({
+        currentPage:nextPage
+      });
+      const offSetX = nextPage * Dimensions.get('window').width;
+      this.refs.scrollView.scrollResponderScrollTo({x:offSetX,y:0,animated:true})
+    },2000)
+  };
+
+  _renderRow =(rowData,sectionID, rowID) => {
+    return(
+      <TouchableHighlight onPress={()=>Alert.alert('商品',null,null)}>
+        <View style={styles.row}>
+          <Text>{rowData}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          backgroundColor={'blue'}
+       <View style={styles.container}>
+         <StatusBar backgroundColor={'blue'}
           barStyle={'default'}
           networkActivityIndicatorVisible={true}
-        />
-        <View style={styles.searchbar}>
-          <TextInput
-            style={styles.input}
-            placeholder='搜索商品'/>
-          <Button style={styles.button}
-            title='搜索'
-            onPress={()=> Alert.alert('你点击了搜索按钮',null,null)}
-          />
-        </View>
-        <View style={styles.advertisement}>
-          <ScrollView
+         />
+         <View style = {styles.searchbar}>
+           <TextInput style={styles.input} placeholder='搜索商品'/>
+           <Button style={styles.button} title='搜索' onPress={()=>Alert.alert('搜索',null,null)}/>
+         </View>
+         <View style={styles.advertisement}>
+           <ScrollView
             ref='scrollView'
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled={true}
-          >
-            <TouchableHighlight onPress={() => Alert.alert('你点击了轮播图',null,null)}>
-
-            </TouchableHighlight>
-          </ScrollView>
-
-        </View>
-
+            horizontal={true} //设置滚动方向为横向，默认为纵向
+            showsHorizontalScrollIndicator={false}   //是否现实滚动条
+            pagingEnabled={true}  //分页
+           >
+             <TouchableHighlight onPress={()=>Alert.alert('广告1',null,null)}>
+               <Text style={{
+                 width:Dimensions.get('window').width, //宽度设置为屏幕宽度
+                 height:180,
+                 backgroundColor:'gray'
+               }}>广告1</Text>
+             </TouchableHighlight>
+             <TouchableHighlight onPress={()=>Alert.alert('广告2',null,null)}>
+               <Text style={{
+                 width:Dimensions.get('window').width,
+                 height:180,
+                 backgroundColor:'orange'
+               }}>广告2</Text>
+             </TouchableHighlight>
+             <TouchableHighlight onPress={()=>Alert.alert('广告3',null,null)}>
+               <Text style={{
+                 width:Dimensions.get('window').width,
+                 height:180,
+                 backgroundColor:'yellow'
+               }}>广告3</Text>
+             </TouchableHighlight>
+           </ScrollView>
+         </View>
+         <View style={styles.products}>
+           <ListView dataSource={this.state.dataSource} renderRow={this._renderRow}/>
+         </View>
+       </View>
     );
   }
 }
@@ -75,26 +120,32 @@ export default class FlexBox extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'gray' ,
-    flexDirection:'row',//设置子组建横向排列，默认是纵向排列
-    //flexWrap:'wrap', //设置子组建溢出的时候换行，默认不换行
-    //justifyContent:'center',//子组建的水平位置
-    //alignItems:'center',//子组建垂直位位置
   },
-  view1:{
-    flex:1, //设置组建大小 1就是100%
-    height:150,
-    width:150,
-    backgroundColor:'red',
-  }  ,
-  view2:{
+  searchbar:{
+    marginTop: Platform.OS === 'ios' ? 20 :0 ,
+    height:40,
+    flexDirection:'row',
+  },
+  input:{
     flex:1,
-    height:150,
-    width:150,
-    backgroundColor:'green' ,
-    //alignSelf:'center',//设置自身垂直位置
-
+    borderColor:'gray',
+    borderWidth:2
+  },
+  button:{
+    flex:1
+  },
+  advertisement:{
+    height:180,
+  },
+  products:{
+    flex:1,
+  },
+  row:{
+    height:60,
+    justifyContent:'center',
+    alignItems:'center',
   }
+
+
 });
 
-AppRegistry.registerComponent('FlexBox',()=> FlexBox);
